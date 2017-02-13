ab7ea725c5ea897f557ac6b894de1eff8a544a1a
Joshua V Sherman <joshua.v.sherman@gmail.com>
2017-02-13 13:26:56 -0500

    implemented login and dashboard

--- src/dashboard-routes/dashboard.js
+++ src/dashboard-routes/dashboard.js
@@ -3,7 +3,7 @@ import {inject} from 'aurelia-framework';
 import {App} from '../app';
 import {Router} from 'aurelia-router';
 import {AuthService} from 'aurelia-auth';
-import {HttpClient, json} from 'aurelia-fetch-client';
+import {HttpClient} from 'aurelia-fetch-client';
 
 @inject(AuthService, HttpClient, App, Router)
 export class Dashboard {
@@ -13,11 +13,11 @@ export class Dashboard {
     this.httpClient = httpClient;
     this.router = router;
   }
-  
+
   authenticated=false;
   firstTimeInfo = false;
   types=['Charity', 'Volunteer'];
-  
+
   getUser(){
     this.authenticated = this.auth.isAuthenticated();
     let uid = this.auth.getTokenPayload().sub;
@@ -25,41 +25,42 @@ export class Dashboard {
     .then(response => response.json())
     .then(data => {
       this.user = data;
-      this.firstTimeInfo = this.configured();
-      if (this.user.userType === 'Charity'){
+      // this.firstTimeInfo = this.configured();
+      if (this.user.userType === 'Librarian'){
         this.user.userType = 1;
-        this.router.navigate('charity');
-      } else if (this.user.userType === 'Volunteer'){
-        this.user.userType = 2;
-        this.router.navigate('volunteer');
+        this.router.navigate('librarian');
       }
+      // } else if (this.user.userType === 'Volunteer'){
+      //   this.user.userType = 2;
+      //   this.router.navigate('volunteer');
+      // }
     });
   }
-  
-  updateUser(){
-    let uid = this.auth.getTokenPayload().sub;
-    let tempUserType = this.user.userType;
-    this.user.userType = this.types[this.user.userType - 1];
-    this.httpClient.fetch(process.env.BackendUrl + '/user/' + uid, {
-      method: 'put',
-      body: json(this.user)
-    })
-    .then(response=>response.json())
-    .then(data=> {
-      this.user.userType = tempUserType;
-      this.getUser();
-    });
-  }
-  
-  configured(){
-    let returnVal = false;
-    if (!('userType' in this.user)){
-      returnVal = true;
-      return returnVal;
-    }
-    return returnVal;
-  }
-  
+
+  // updateUser(){
+  //   let uid = this.auth.getTokenPayload().sub;
+  //   let tempUserType = this.user.userType;
+  //   this.user.userType = this.types[this.user.userType - 1];
+  //   this.httpClient.fetch(process.env.BackendUrl + '/user/' + uid, {
+  //     method: 'put',
+  //     body: json(this.user)
+  //   })
+  //   .then(response=>response.json())
+  //   .then(data=> {
+  //     this.user.userType = tempUserType;
+  //     this.getUser();
+  //   });
+  // }
+
+  // configured() {
+  //   let returnVal = false;
+  //   if (!('userType' in this.user)){
+  //     returnVal = true;
+  //     return returnVal;
+  //   }
+  //   return returnVal;
+  // }
+
   activate() {
     this.getUser();
   }
