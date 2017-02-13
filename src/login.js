6405235685bb3941c4d5d9fa19d2f0a7fc51e9fe
Joshua V Sherman <joshua.v.sherman@gmail.com>
2017-02-13 14:53:12 -0500

    updated index.html for google verification

--- src/login.js
+++ src/login.js
@@ -1,20 +1,19 @@
 
 import {AuthService} from 'aurelia-auth';
 import {inject} from 'aurelia-framework';
-import{App} from "./app";
+import {App} from './app';
 @inject(AuthService, App )
 export class Login {
   constructor(auth, app){
     this.auth = auth;
     this.app = app;
-  };
+  }
 
   authenticate(name){
     return this.auth.authenticate(name, false, null)
     .then((response)=>{
       this.auth.setToken(response.token);
       this.app.authenticated = this.auth.isAuthenticated();
-
     });
   }
 }
