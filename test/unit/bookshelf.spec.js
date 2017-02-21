ad6ad38cba2a03b4b417bd9555982af2eb52ee86
Olivia Thompson <ort2411@gmail.com>
2017-02-21 11:16:50 -0500

    createBooks test complete

--- test/unit/bookshelf.spec.js
+++ test/unit/bookshelf.spec.js
@@ -1,5 +1,5 @@
 import {Bookshelf} from '../../src/bookshelf';
-
+const Counter = require('assertions-counter');
 class HttpStub {
   fetch(fn) {
     var response = this.itemStub;
@@ -8,20 +8,53 @@ class HttpStub {
       resolve({ json: () => response });
     });
   }
+  configure(fn) {
+    this.__configureCallback = fn;
+    return this.__configureReturns;
+  }
 }
 describe('the bookshelf module', () => {
-  it('gets all books', () => {
+  var bookshelf1;
+  beforeEach(() => {
     var itemStubs = [1];
-    var itemFake = [2];
+    //var itemFake = [2];
     var http = new HttpStub();
     http.itemStub = itemStubs;
-
-    let bookshelf1 = new Bookshelf(http);
+    bookshelf1 = new Bookshelf(http);
+  });
+  it('gets all books', () => {
+    //var itemStubs = [1];
+    //var itemFake = [2];
+    //var http = new HttpStub();
+    //http.itemStub = itemStubs;
+    //let bookshelf1 = new Bookshelf(http);
 
     bookshelf1.activate().then(() => {
       expect(bookshelf1.books).toBe(itemStubs);
-      expect(bookshelf1.books).not.toBe(itemFake);
+      //expect(bookshelf1.books).not.toBe(itemFake);
       done();
+    });
+  });
+
+  it('tests configHttpClient', (done) => {
+    //var http = new HttpStub();
+    //http.itemStub = itemStubs;
+    //let bookshelf1 = new Bookshelf(http);
+    const { add: ok } = new Counter(2, done);
+    //bookshelf1.auth.tokenInterceptor = 'tokenInterceptor';
+    bookshelf1.activate().then(() => {
+      bookshelf1.httpClient.__configureCallback(new(class {
+        withBaseUrl(opts) {
+          expect(opts).toBe(process.env.BackendUrl);
+          ok();
+          return this;
+        }
+        useStandardConfiguration() {
+          //   expect(token).toBe(app1.auth.tokenInterceptor);
+          ok();
+          return this;
+        }
+      })());
     });
   });
 });
