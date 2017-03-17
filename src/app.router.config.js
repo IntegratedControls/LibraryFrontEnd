72fe891740fa889e61a7878307a99e6af24b0955
Joshua V Sherman <joshua.v.sherman@gmail.com>
2017-03-17 11:20:18 -0400

    updated home and login pages with additional user instruction

--- src/app.router.config.js
+++ src/app.router.config.js
@@ -18,7 +18,7 @@ export class AppRouterConfig{
       config.addPipelineStep('authorize', AuthorizeStep);//Is the actually Authorization. Prevents users from certain sites when not authorized.
       config.map([
         { route: ['', 'home'], name: 'home', moduleId: './home', nav: true, title: 'Home', settings: 'fa fa-home'},
-        { route: 'bookshelf', name: 'bookshelf', moduleId: './bookshelf', nav: true, title: 'Book Shelf', settings: 'fa fa-book'},
+        { route: 'bookshelf', name: 'bookshelf', moduleId: './bookshelf', nav: true, title: 'Bookshelf', settings: 'fa fa-book'},
         // { route: 'createbooks', name: 'createbooks', moduleId: './createBooks', nav: false, title: 'Create Books', settings: 'fa fa-plus'},
         { route: 'login', name: 'login', moduleId: './login', nav: false, title: 'Login', settings: 'fa fa-sign-in'},
         { route: 'dashboard', name: 'dashboard-router', moduleId: './dashboard-router', nav: false, title: 'Dashboard', auth: true, settings: 'fa fa-tachometer'},
