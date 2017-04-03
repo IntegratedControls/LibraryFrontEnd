c9ce7e0ea62588844dab7ceff02ce2e83000924d
Joshua V Sherman <joshua.v.sherman@gmail.com>
2017-04-03 10:07:23 -0400

    rearranged home screen: put pic on right side if widescreen

--- src/home.js
+++ src/home.js
@@ -1,28 +1,20 @@
-//import {computedFrom} from 'aurelia-framework';
+import {bindable} from 'aurelia-framework';
 
 export class Home {
-  // heading = 'Welcome to the Aurelia Navigation App!';
-  // firstName = 'John';
-  // lastName = 'Doe';
-  // previousValue = this.fullName;
 
-  //Getters can't be directly observed, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
-  //as well as the corresponding import above.
-  //@computedFrom('firstName', 'lastName')
-  // get fullName() {
-  //   return `${this.firstName} ${this.lastName}`;
-  // }
-  //
-  // submit() {
-  //   this.previousValue = this.fullName;
-  //   alert(`Welcome, ${this.fullName}!`);
-  // }
-  //
-  // canDeactivate() {
-  //   if (this.fullName !== this.previousValue) {
-  //     return confirm('Are you sure you want to leave?');
-  //   }
-  // }
+  @bindable
+  columnWidth = '450px';
+
+  get widescreen(){
+    let iswidescreen = false;
+    let currentscreenwidth = document.documentElement.clientWidth;
+    /* istanbul ignore else */
+    if (currentscreenwidth > 766){
+      iswidescreen = true;
+      this.columnWidth = '450px';
+    } else {
+      this.columnWidth = 'auto';
+    }
+    return iswidescreen;
+  }
 }
